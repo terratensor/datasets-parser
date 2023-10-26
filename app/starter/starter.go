6b6980b4123f08b5ae9f4097060a1621dbe9ffc3
agusev <audetv@gmail.com>
2023-10-26 23:09:09 +0300

    Added process Ancient Locations al_sites.csv

--- app/starter/starter.go
+++ app/starter/starter.go
@@ -122,6 +122,12 @@ func getEntriesInstance(entries dataset.Store, folder string, filename string) (
 			return nil, err
 		}
 		return ne, nil
+	case "Ancient Locations al_sites.csv":
+		ne, err := ancienthuman.NewCSVEntries(fmt.Sprintf("%v/%v", folder, filename))
+		if err != nil {
+			return nil, err
+		}
+		return ne, nil
 	default:
 		return nil, fmt.Errorf("%v file not supported", filename)
 	}
