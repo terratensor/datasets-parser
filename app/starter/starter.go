b11ac58b56f945e0e676f4dde6b3afb3700e7219
agusev <audetv@gmail.com>
2023-10-27 23:03:37 +0300

    Fixed All_ancient_human_dna.csv

--- app/starter/starter.go
+++ app/starter/starter.go
@@ -158,6 +158,12 @@ func getEntriesInstance(entries dataset.Store, folder string, filename string) (
 			return nil, err
 		}
 		return ne, nil
+	case "Historical Cities.csv":
+		ne, err := ancienthuman.NewCSVEntries(fmt.Sprintf("%v/%v", folder, filename))
+		if err != nil {
+			return nil, err
+		}
+		return ne, nil
 	default:
 		return nil, fmt.Errorf("%v file not supported", filename)
 	}
