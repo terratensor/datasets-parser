0bbcbf376b0146b839e09f5417af60b19fbb1898
agusev <audetv@gmail.com>
2023-10-27 23:29:22 +0300

    Added Rank 1 Archaeology Sites.csv

--- app/starter/starter.go
+++ app/starter/starter.go
@@ -182,6 +182,12 @@ func getEntriesInstance(entries dataset.Store, folder string, filename string) (
 			return nil, err
 		}
 		return ne, nil
+	case "Rank 1 Archaeology Sites.csv":
+		ne, err := ancienthuman.NewCSVEntries(fmt.Sprintf("%v/%v", folder, filename))
+		if err != nil {
+			return nil, err
+		}
+		return ne, nil
 	default:
 		return nil, fmt.Errorf("%v file not supported", filename)
 	}
