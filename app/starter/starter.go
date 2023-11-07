0d681712b676e99bb6c18458daa09b190e48276c
agusev <audetv@gmail.com>
2023-11-07 13:57:54 +0300

    Added Полезные ископаемые мира.csv

--- app/starter/starter.go
+++ app/starter/starter.go
@@ -212,6 +212,12 @@ func getEntriesInstance(entries dataset.Store, folder string, filename string) (
 			return nil, err
 		}
 		return ne, nil
+	case "Полезные ископаемые мира.csv":
+		ne, err := ancienthuman.NewCSVEntries(fmt.Sprintf("%v/%v", folder, filename))
+		if err != nil {
+			return nil, err
+		}
+		return ne, nil
 	default:
 		return nil, fmt.Errorf("%v file not supported", filename)
 	}
